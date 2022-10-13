996b4b0fc91143789c04437e23bade9294b2deec
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-13 16:16:06 +0300

    add

---
+++ resources/views/livewire/Create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<div>
+    <form>
+    <div class="form-group">
+        <label for="exampleFormControlInput1">Title</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Title" wire:model="title">
+        @error('title') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+    <div class="form-group">
+        <label for="exampleFormControlInput2">Description</label>
+        <input type="text" class="form-control" id="exampleFormControlInput2" wire:model="description" placeholder="Enter Description">
+        @error('description') <span class="text-danger">{{ $message }}</span>@enderror
+    </div>
+    <button wire:click.prevent="store()" class="btn btn-success">Save</button>
+</form>
+</div>
+</body>
+</html>
