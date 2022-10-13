23ebdee2998a377373b91dddc14fc95a170b57c5
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-13 16:37:17 +0300

    add

---
+++ app/Models/Entry.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Entry extends Model
+{
+    protected  $fillable = [
+        'status_id',
+        'name'
+    ];
+    use HasFactory;
+}
