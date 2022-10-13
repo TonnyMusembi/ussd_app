c37b4202b90092a513117bf7fda493fc11ec5633
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-13 11:45:12 +0300

    add

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FileController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +20,9 @@ Route::get('/', function () {
 });
 Route::get('user-pagination', function () {
     return view('default');
+});
+
+Route::controller(FileController::class)->group(function(){
+    Route::get('file-upload', 'index');
+    Route::post('file-upload', 'store')->name('file.upload');
 });
