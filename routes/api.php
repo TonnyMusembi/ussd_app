0f1c7b387d0f9b25e21d7634646000163fd85c50
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-17 10:48:26 +0300

    add

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TestController;
 use App\Http\Controllers\UssdController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -20,3 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('ussd-session', [UssdController::class,'session']);
+Route::resource('tests',TestController::class);
+
