277787480a71e10c402ca1f5cc61ec920023945e
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-18 12:29:20 +0300

    add

---
+++ app/Http/Controllers/PredictionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Prediction;
+use Illuminate\Http\Request;
+
+class PredictionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Prediction  $prediction
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Prediction $prediction)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Prediction  $prediction
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Prediction $prediction)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Prediction  $prediction
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Prediction $prediction)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Prediction  $prediction
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Prediction $prediction)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_10_18_071452_create_predictions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePredictionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('predictions', function (Blueprint $table) {
+            // $table->id();
+            $table->integerIncrements('prediction_id');
+            $table->string('name');
+            $table->string('league');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('predictions');
+    }
+}
+
