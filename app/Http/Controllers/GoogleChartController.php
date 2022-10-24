28c3d9de7ec0dd7006957e033545f8f9aa941598
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-24 14:53:06 +0300

    add

--- app/Http/Controllers/GoogleChartController.php
+++ app/Http/Controllers/GoogleChartController.php
@@ -15,13 +15,17 @@ class GoogleChartController extends Controller
     public function index(){
         $users = User::select(DB::raw("COUNT(*) as count"), DB::raw("MONTHNAME(created_at) as month_name"))
                     ->whereYear('created_at', date('Y'))
-                    ->groupBy(DB::raw("Month(created_at)"))
-                    ->pluck('count', 'month_name');
+                    ->groupBy(DB::raw("Month(created_at)"));
+                    // ->get();
+                    // ->pluck('count', 'month_name');
 
         return view('chart', compact('users'));
 
     }
     public function store( Request $request ){
+
+    }
+    public function  destroy($id){
 
     }
 
