32b1c876da3778bc4b23477b152b61c52788a1e3
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-25 00:00:53 +0300

    add

---
+++ resources/views/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Validate</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container mt-5" style="max-width: 500px">
+        <form action="{{ route('validate.exists') }}" method="post">
+            @csrf
+            <div class="form-group">
+                <input class="form-control" name="title" value="{{ old('title') }}">
+
+                @if($errors->has('title'))
+                  <span class="text-danger">{{ $errors->first('title') }}</span>
+                @endif
+            </div>
+            <div class="d-grid mt-3">
+                <button class="btn btn-success">Submit</button>
+            </div>
+        </form>
+    </div>
+
+
+    <div class="container mt-5" style="max-width: 500px">
+        <div class=" card-body">
+            <div class="card-header">Data</div>
+            <form action="">
+                <div class="d-grid mt-3">
+                 <button class="btn btn-success">Pull</button>
+                </div>
+            </form>
+
+        </div>
+
+    </div>
+</body>
+</html>
