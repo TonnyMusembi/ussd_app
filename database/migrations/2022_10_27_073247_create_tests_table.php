f275ff07b0cea9c3c1afa6b02d32daaafde17f04
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-25 15:46:03 +0300

    add

---
+++ database/migrations/2022_10_27_073247_create_tests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tests', function (Blueprint $table) {
+            // $table->id();
+            $table->integerIncrements('test_id')->unique();
+            $table->string('status');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tests');
+    }
+}
