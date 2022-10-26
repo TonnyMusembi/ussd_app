34d5e3847cc86d306d0e72bb9e610ec970e05366
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-26 17:10:40 +0300

    add

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Notification extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'notitification_id',
+        'notificartion_type'
+    ];
 }
