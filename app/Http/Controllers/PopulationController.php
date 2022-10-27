bc1cd29801cfb63434882de20a15684350fc9089
TonnyMusembi <musembutonnyj@gmail.com>
2022-10-27 13:09:50 +0300

    add

---
+++ app/Http/Controllers/PopulationController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Population;
+use Illuminate\Http\Request;
+
+class PopulationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Population  $population
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Population $population)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Population  $population
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Population $population)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Population  $population
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Population $population)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Population  $population
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Population $population)
+    {
+        //
+    }
+}
