d63eee332f44039ea015a3c132cad4b3ad6ac180
TonnyMusembi <musembutonnyj@gmail.com>
2023-04-07 10:54:56 +0300

    add

---
+++ app/Http/Livewire/UserTableDemo.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UserTableDemo extends Component
+{
+    public function render()
+    {
+        return view('livewire.user-table-demo');
+    }
+}
